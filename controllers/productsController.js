bc9b26be4f14976c18cfc3553ec7991554d87127
EdgarSago <edgar13sago@gmail.com>
2024-03-09 14:31:25 -0600

    sort, fields, page, limit for getAllProducts

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productsModel')
+const User = require('../models/usersModel')
 
 const asyncHandler = require('express-async-handler')
 const slugify = require('slugify')
@@ -55,7 +56,45 @@ const getProduct = asyncHandler(async (req, res) => {
 
 const getAllProducts = asyncHandler(async (req, res) => {
     try {
-        const getProducts = await Product.find()
+        // Filtering
+        const queryObj = { ...req.query }
+        const excludeFields = ['page', 'sort', 'limit', 'fields']
+        excludeFields.forEach((el) => delete queryObj[el])
+        console.log(queryObj)
+
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+        
+        let queryProduct = Product.find(JSON.parse(queryStr))
+
+        // Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            queryProduct = queryProduct.sort(sortBy)
+        } else {
+            queryProduct = queryProduct.sort('-createdAt')
+        }
+
+        // Limiting the fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ')
+            queryProduct = queryProduct.select(fields)
+        } else {
+            queryProduct = queryProduct.select('-__v')
+        }
+
+        // Pagination
+        const page = req.query.page
+        const limit = req.query.limit
+        const skip = (page - 1) * limit
+        queryProduct = queryProduct.skip(skip).limit(limit)
+
+        if (req.query.page) {
+            const productCount = await Product.countDocuments()
+            if (skip >= productCount) throw new Error('This Page does not exists')
+        }
+        
+        const getProducts = await queryProduct
         res.json(getProducts)
     } catch (error) {
         throw new Error(error)
