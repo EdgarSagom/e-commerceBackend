567ca2d313d82c0be3af57240e31a13fde37483a
EdgarSago <edgar13sago@gmail.com>
2024-03-09 23:13:24 -0600

    rating functionality

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -134,11 +134,63 @@ if (alreadyAdded) {
     }
 })
 
+const rating = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const { star, comment, prodId } = req.body
+    
+    try {
+        const product = await Product.findById(prodId)
+        let alreadyRated = product.ratings.find((userId) => userId.postedby.toString() === _id.toString())
+
+        if (alreadyRated) {
+            const updateRating = await Product.updateOne({
+                ratings: {
+                    $elemMatch: alreadyRated
+                }
+            },{
+                $set: {
+                    'ratings.$.star': star,
+                    'ratings.$.comment': comment
+                }
+            },{
+                new: true
+            })
+        } else {
+            const rateProduct = await Product.findByIdAndUpdate(prodId, {
+                $push: {
+                    ratings: {
+                        star: star,
+                        comment: comment,
+                        postedby: _id
+                    }
+                }
+            },{
+                new: true
+            })
+        }
+
+        const getAllRatings = await Product.findById(prodId)
+        let totalRating = getAllRatings.ratings.length
+        let sumRating = getAllRatings.ratings.map((item) => item.star).reduce((prev, curr) => prev + curr, 0)
+        let actualRating = Math.round(sumRating / totalRating)
+        let finalProduct = await Product.findByIdAndUpdate(prodId, {
+            totalRating: actualRating
+        },{
+            new: true
+        })
+        res.json(finalProduct)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
 module.exports = {
     createProduct,
     getProduct,
     getAllProducts,
     updateProduct,
     deleteProduct,
-    addToWishList
+    addToWishList,
+    rating
 }
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { createProduct, getProduct, getAllProducts, updateProduct, deleteProduct, addToWishList } = require('../controllers/productsController')
+const { createProduct, getProduct, getAllProducts, updateProduct, deleteProduct, addToWishList, rating } = require('../controllers/productsController')
 const { protect, isAdmin } = require('../middleware/authMiddleware')
 
 router.post('/', protect, isAdmin, createProduct)
 router.get('/:id', getProduct)
 router.put('/wishlist', protect, addToWishList)
+router.put('/rating', protect, rating)
 router.put('/:id', protect, isAdmin, updateProduct)
 router.delete('/:id', protect, isAdmin, deleteProduct)
 router.get('/', getAllProducts)
