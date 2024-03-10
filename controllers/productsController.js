9d6c9160a40b66ace0ffef769a73077a7f60898e
EdgarSago <edgar13sago@gmail.com>
2024-03-09 20:34:20 -0600

    wishlist functionality

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,6 +3,7 @@ const User = require('../models/usersModel')
 
 const asyncHandler = require('express-async-handler')
 const slugify = require('slugify')
+const validateMongoDbId = require('../utils/validateMongoDbId')
 
 const createProduct = asyncHandler(async (req, res) => {
     try {
@@ -18,6 +19,7 @@ const createProduct = asyncHandler(async (req, res) => {
 
 const updateProduct = asyncHandler(async (req, res) => {
     const id = req.params._id
+    validateMongoDbId(id)
 
     try {
         if (req.body.title) {
@@ -35,6 +37,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 const deleteProduct = asyncHandler(async (req, res) => {
     const id = req.params.id
+    validateMongoDbId(id)
 
     try {
         const deleteProd = await Product.findOneAndDelete(id)
@@ -46,6 +49,8 @@ const deleteProduct = asyncHandler(async (req, res) => {
 
 const getProduct = asyncHandler(async (req, res) => {
     const { id } = req.params
+    validateMongoDbId(id)
+
     try {
         const findProduct = await Product.findById(id)
         res.json(findProduct)
@@ -101,10 +106,39 @@ const getAllProducts = asyncHandler(async (req, res) => {
     }
 })
 
+const addToWishList = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const { prodId } = req.body
+
+    try {
+        const user = await User.findById(_id)
+        const alreadyAdded = user.wishlist.find((id) => id.toString() === prodId)
+
+if (alreadyAdded) {
+    let user = await User.findByIdAndUpdate(_id, {
+        $pull: { wishlist: prodId}
+    },{
+        new: true
+    })
+    res.json(user)
+} else {
+    let user = await User.findByIdAndUpdate(_id, {
+        $push: { wishlist: prodId}
+    },{
+        new: true
+    })
+    res.json(user)
+}
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
 module.exports = {
     createProduct,
     getProduct,
     getAllProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    addToWishList
 }
